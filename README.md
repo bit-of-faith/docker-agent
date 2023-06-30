b5ffb609c19db7171f993d2cc2c5ec38a3bdd0c7
Joshua Hegedus <jhegedus9@gmail.com>
2023-06-30 19:43:04 +0200

    universal init

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Development
 Build: `docker build -t  ghcr.io/bit-of-faith/docker-agent:latest .`
 Push: `docker push ghcr.io/bit-of-faith/docker-agent:latest`
+
+## Init Usage
+
+Demo run: `.\init.ps1 -downloadUrl "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/5.13.0.66756/sonar-scanner-msbuild-5.13.0.66756-net46.zip" -sonarToken "sqp_3356fb7d74b8852328a827ce0d612f559a4bdd39" -target "sonar-scanner-msbuild-net46"`
--- init.ps1
+++ init.ps1
@@ -1,6 +1,14 @@
 param (
     [parameter(Mandatory=$false)]
-        [string]$dir = ".\"
+    [string]$dir = ".\",
+    [parameter(Mandatory=$true)]
+    [string]$downloadUrl,
+    [parameter(Mandatory=$false)]
+    [string]$sonarUrl = "https://sonar.bitof.faith",
+    [parameter(Mandatory=$false)]
+    [string]$target = "sonar-scanner",
+    [parameter(Mandatory=$true)]
+    [string]$sonarToken
 )
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -15,20 +23,25 @@ function Unzip
 # $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 # $isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-Invoke-WebRequest -OutFile "${dir}sonar-scanner.zip" -Uri "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/5.13.0.66756/sonar-scanner-msbuild-5.13.0.66756-net5.0.zip"
-if (!(Test-Path -Path "C:\sonar-scanner")) {
-    Unzip "sonar-scanner.zip" "C:\sonar-scanner"
+Invoke-WebRequest -OutFile "${dir}${target}.zip" -Uri $downloadUrl
+if (!(Test-Path -Path "C:\${target}")) {
+    Unzip "${target}.zip" "C:\${target}"
 }
 
-$testpath = "${dir}SonarQube.Analysis.xml" ;$path=Get-ChildItem $testpath -Recurse
-$path.fullname | ForEach-Object {(Get-Content $_ -Raw).Replace('SONAR_URL', $Env:SONAR_URL)| Set-Content $_}
-$path.fullname | ForEach-Object {(Get-Content $_ -Raw).Replace('SONAR_TOKEN', $Env:SONAR_TOKEN)| Set-Content $_}
+$testpath = "${dir}\SonarQube.Analysis.xml" ;$path=Get-ChildItem $testpath -Recurse
+$path.fullname | ForEach-Object {(Get-Content $_ -Raw).Replace('SONAR_URL', $sonarUrl)| Set-Content $_}
+$path.fullname | ForEach-Object {(Get-Content $_ -Raw).Replace('SONAR_TOKEN', $sonarToken)| Set-Content $_}
 
-Copy-Item "${dir}SonarQube.Analysis.xml" -Destination "C:\sonar-scanner\SonarQube.Analysis.xml" -force
-if (!([Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) -like "*C:\sonar-scanner*")) { 
+Copy-Item "${dir}SonarQube.Analysis.xml" -Destination "C:\${target}\SonarQube.Analysis.xml" -force
+
+$testpath = "${dir}\SonarQube.Analysis.xml" ;$path=Get-ChildItem $testpath -Recurse
+$path.fullname | ForEach-Object {(Get-Content $_ -Raw).Replace($sonarUrl, "SONAR_URL")| Set-Content $_}
+$path.fullname | ForEach-Object {(Get-Content $_ -Raw).Replace($sonarToken, "SONAR_TOKEN")| Set-Content $_}
+
+if (!([Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) -like "`*C:\${target}*")) { 
     [Environment]::SetEnvironmentVariable(
     "Path", 
-    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + "C:\sonar-scanner;", 
+    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + "C:\${target};", 
     [System.EnvironmentVariableTarget]::Machine)
 }
 
